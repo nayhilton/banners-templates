fbbbd3f2afaddd7c6e1c508d3b1eafccf5e1b983
Nayara Hilton <nayara.hilton@fbiz.com.br>
2017-05-26 11:40:35 -0300

    generate content working

--- npm/generatecontent.js
+++ npm/generatecontent.js
@@ -4,46 +4,42 @@ var mkdir = require("mkdirp");
 var copydir = require('copy-dir');
 var path = require('path');
 var dataFile = 'src/data/data.json';
-var templateFile = 'src/templates/masterhead/index.hbs';
 var handlebars = require('handlebars');
 
 (function path() {
+	var data = JSON.parse(fs.readFileSync(dataFile));
 
-	(!fs.existsSync('./output')) ? fs.mkdirSync('./output') : '';
+	glob("src/templates/*", function (er, folder) { //Get all templates
 
-	(!fs.existsSync('./output/templates')) ? fs.mkdirSync('./output/templates'): '';
-	
+		var templateFolders = folder; //Array of template folders
 
-	console.log('passou');
+		templateFolders.forEach(function(item) {
 
-	//O que falta:
+			var templateFile = item + '/' + '/index.html', //Every file of every template folder 
+			file = fs.readFileSync(templateFile).toString(), //convert every file to string		
+			template = handlebars.compile(file); //set its own data
 
-	//1. Deixar Templates dinamicos
+			for(var prop in data) {
 
-	//2. Excluir hbs
+				var bannerType = item.replace('src/templates/', '');
 
-	var file = fs.readFileSync(templateFile).toString(),
-		data = JSON.parse(fs.readFileSync(dataFile)),
-		template = handlebars.compile(file);		
+				console.log('bannerType', bannerType);
 
-		for(var prop in data) {
-			// console.log('Chaves', prop, 'Prop', data[prop]); //Chaves
+				copydir.sync('src/templates/masterhead', 'output/templates/' + bannerType + '/' + prop); //Copy unchanged files
 
-			var destFile = 'output/templates/' + prop + '/index.html';
+				var destFile = 'output/templates/' + bannerType + '/' + prop + '/index.html'; 
 
-			if (!fs.existsSync('output/templates/' + prop)) {
-			    fs.mkdirSync('output/templates/' + prop);
+				(!fs.existsSync('output/templates/' + bannerType + '/' + prop)) ? fs.mkdirSync('output/templates/' + item + '/' + prop) : '';
+
+				fs.writeFile(destFile, ''); //Clear dest file
+
+				var result = template(data[prop]); //Set data
+
+				fs.appendFile(destFile, result, function(error) {
+					(error) ? console.error("Error") : console.log("Successful Write to " + destFile);		
+				});	
 			}
 
-			fs.writeFile(destFile, '');
-
-			var result = template(data[prop]);
-
-			copydir.sync('src/templates/masterhead', 'output/templates/' + prop);
-
-			fs.appendFile(destFile, result, function(error) {
-				(error) ? console.error("Error") : console.log("Successful Write to " + destFile);		
-			});	
-		}	
-
+		});	
+	});
 })();
